5664f5f96b73d7b0dd7bba4b4d691e395a787bce
Jonathan Leonhardt <jonathan.arai@gmail.com>
2022-08-13 18:45:34 -0300

    primeira versão da tela de conteudos

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -7,6 +7,24 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
+  public posts = [
+    {
+      title: 'Lorem Ipsum Dolor',
+      description: 'Neque porro quisquam est qui dolorem',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sagittis est vel enim venenatis ultricies.'
+    },
+    {
+      title: 'Neque porro quisquam',
+      description: 'Etiam sagittis est vel enim venenatis ultricies',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sagittis est vel enim venenatis ultricies.'
+    },
+    {
+      title: 'Fusce tincidunt euismod ex?',
+      description: 'Neque porro quisquam est qui dolorem',
+      content: 'Aenean non purus nunc. Morbi eu quam odio. Pellentesque tincidunt ornare nisl, quis volutpat nisi efficitur in. Integer.'
+    }
+  ];
+
   constructor() {}
 
 }
